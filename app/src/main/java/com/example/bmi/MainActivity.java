886f4d5fd4e2d1f5a4e91fc2be15571a67341754
Hank <111b150005@mailst.cjcu.edu.tw>
2024-03-13 15:50:45 +0800

    計算BMI

--- app/src/main/java/com/example/bmi/MainActivity.java
+++ app/src/main/java/com/example/bmi/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void calcMBI(View view) {
+        EditText height  = findViewById(R.id.etHeight);
+        EditText weight = findViewById(R.id.etWeight);
+        double h = Double.parseDouble(height.getText().toString());
+        double w = Double.parseDouble(weight.getText().toString());
+        double bmi = w / (h/100.0 * h/100.0);
+        String result = "你的BMI是：" + bmi;
+
+        TextView show = findViewById(R.id.tvShow);
+        show.setText(result);
     }
 }
